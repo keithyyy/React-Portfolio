ee46f5917fb12573ed62360d89e1ecfbdaea8904
Keith Crooc <keith.crooc@gmail.com>
2021-03-06 20:23:59 -0500

    deploying again

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ function App() {
     <Router>
       <div className="container main">
         <Navbar />
-        <Route exact path="#/" component={Home} />
-        <Route exact path="#/projects" component={Projects} />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/projects" component={Projects} />
 
         
       </div>
